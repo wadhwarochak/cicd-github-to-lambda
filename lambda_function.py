7462b56b459e2f22dc71a59eb2bfb57fc47d5b70
wadhwarochak <rochak.wadhwa@gmail.com>
2026-05-08 06:29:51 +0530

    Added yaml

--- lambda_function.py
+++ lambda_function.py
@@ -7,8 +7,7 @@ def lambda_handler(event, context):
     print("Event Data -> ", event)
     response = requests.get("https://www.google.com/")
     print(response.text)
-
-
+    # test
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
